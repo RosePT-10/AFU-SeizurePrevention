56c4a8cf6e668d31b6cb614439737c62d0c159d9
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-06 15:04:42 -0600

    EMP fixed

--- AFUSeizurePrevention/Core.cs
+++ AFUSeizurePrevention/Core.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using HarmonyLib;
 using UnityEngine;
 using Il2CppView_Equipment;
+using Il2CppTMPro;
 
 [assembly: MelonInfo(typeof(AFUSeizurePrevention.Core), "AFUSeizurePrevention", "1.0.0", "taldo", null)]
 [assembly: MelonGame("Videocult", "Airframe")]
@@ -11,6 +12,17 @@ namespace AFUSeizurePrevention
 {
     public class Core : MelonMod
     {
+        
+        [HarmonyPatch(typeof(EMPgrenade_View), "Draw", new Type[] {typeof(float)})]
+        public class EMPFix
+        {
+            public static void Postfix(EMPgrenade_View __instance)
+            {
+                //Melon<Core>.Logger.Msg("detected");
+                __instance.myLight.range = 0;
+            }
+            
+        }
         public override void OnInitializeMelon()
         {
             LoggerInstance.Msg("Initialized.");
@@ -20,9 +32,9 @@ namespace AFUSeizurePrevention
         {
             base.OnUpdate();
 
-            Type my_light_override = typeof(EMPgrenade_View);
-            FieldInfo myLight_field = my_light_override.GetField("myLight");
-            LoggerInstance.Msg(myLight_field.GetValue(new EMPgrenade_View()));
+            //Type my_light_override = typeof(EMPgrenade_View);
+            //FieldInfo myLight_field = my_light_override.GetField("myLight");
+            //LoggerInstance.Msg(myLight_field.GetValue(new EMPgrenade_View()));
 
         }
     }
--- AFUSeizurePrevention/obj/Debug/net6.0/AFUSeizurePrevention.AssemblyInfo.cs
+++ AFUSeizurePrevention/obj/Debug/net6.0/AFUSeizurePrevention.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AFUSeizurePrevention")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12f74716dd08029eb00f5718998864cf1f03e905")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df6d425b675632b17bbe6f6f559adf12c483dd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AFUSeizurePrevention")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AFUSeizurePrevention")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
